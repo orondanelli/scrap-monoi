7c426d7ba6f2434edaf5558aea8741476976220d
orlando rondanelli <orlando.rondanelli@gmail.com>
2022-03-30 08:51:39 -0300

    add falabella file :sparkles:

---
+++ falabella.js
@@ -0,0 +1,21 @@
+let request = require('request');
+let cheerio = require('cheerio');
+
+var url = 'https://www.falabella.com/falabella-cl/category/cat7660002/Belleza--higiene-y-salud?f.product.brandName=monoi%3A%3Amonoi+tiki+tahiti&sred=monoi'
+request(url, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+        let $ = cheerio.load(html);
+        let content = $("div#testId-searchResults-products .grid-pod .pod");
+
+        let response = content.map(function() {
+            return {
+                name: $(this).find('div a span .pod-subTitle').text().trim(),
+                price: $(this).find('a div ol li div .copy10').text().trim(),
+                brand: $(this).find('.pod-title').text().trim()
+            }
+        });
+        //console.log($('.prices ol li[class=prices-0] div span'))
+        console.log(response.toArray());
+
+    }
+});
--- index.js
+++ index.js
@@ -6,10 +6,12 @@ request(url, function (error, response, html) {
     if (!error && response.statusCode == 200) {
         let $ = cheerio.load(html);
         let content = $("ul#search-result-items .main");
+        console.log($("ul#search-result-items .main").html())
         let response = content.map(function() {
             return {
                 name: $(this).find('.name-product-plp span').text().trim(),
                 price: $(this).find('.price__text').text().trim(),
+                brand: $(this).find('.brand-product-plp').text().trim()
             }
         });
 
