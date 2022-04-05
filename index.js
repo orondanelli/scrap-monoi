42b6ad96015e2f179011ea04964fe207043ddc55
orlando rondanelli <orlando.rondanelli@gmail.com>
2022-04-05 19:06:54 -0400

    add sallybeauty scrap

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const sally = require('./lib/scrapers/sallybeauty')
 const products = require('./db/products')
 
 async function scrap() {
-    //let productFalabella = await falabella.getProduct();
-    //let productParis = await paris.getProduct();
+    let productFalabella = await falabella.getProduct();
+    let productParis = await paris.getProduct();
     let productSally = await sally.getProduct();
-    console.log(productSally)
-    //let allProducts = await productFalabella.concat(productParis);
-    //products.createProducts(allProducts);
+    let allProducts = await productFalabella.concat(productParis, productSally);
+    console.log(allProducts)
+    products.createProducts(allProducts);
 
 }
 
